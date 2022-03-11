851938ff8f0ba1a41784e56497e651800f532ea1
Alex Jackson <45179933+ajaxbits@users.noreply.github.com>
2022-03-11 05:24:31 -0600

    misc changes

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ use db::{Pool, Post};
 use r2d2_sqlite::SqliteConnectionManager;
 use serde::Deserialize;
 use std::{fs, path::Path};
+use tinytemplate::TinyTemplate;
 
 const INDEX: &str = "static/index.html";
 const CONTACT: &str = "static/contact.html";
@@ -39,22 +40,29 @@ async fn contact(req: HttpRequest) -> actix_web::Result<HttpResponse> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Connect to SQLite
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
     let manager = SqliteConnectionManager::file("posts.db");
     let pool = Pool::new(manager).unwrap();
 
-    // Serve app
     HttpServer::new(move || {
         App::new()
             .wrap(actix_web::middleware::Compress::default())
             .wrap(actix_web::middleware::Logger::default())
-            .app_data(web::Data::new(pool.clone()))
             .service(index)
             .service(contact)
-            .service(web::scope("/blog").configure(blog::blog_config))
+            .service(
+                web::scope("/blog")
+                    .configure(blog::blog_config)
+                    .app_data(web::Data::new(pool.clone())),
+            )
             .service(web::scope("/admin").configure(admin::admin_config))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
+
+static ERROR: &str = include_str!("../templates/error.html");
+// static INDEX: &str = include_str!("templates/index.html");
