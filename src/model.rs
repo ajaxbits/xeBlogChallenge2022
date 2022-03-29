8d2bccfe7e98069841e348708269cc0c2d8a69df
Alex Jackson <ajaxbits@pm.me>
2022-03-29 14:55:14 -0500

    refine the RawPost from impl to separate out SQL types and abstract Post types more succinctly

--- src/model.rs
+++ src/model.rs
@@ -8,14 +8,13 @@ pub struct Post {
     pub uuid: Uuid,
 
     #[serde(default)]
-    #[serde(deserialize_with = "bool_from_int")]
     pub draft: bool,
 
     pub title: String,
 
     pub date: chrono::NaiveDate,
 
-    #[serde(default = "default_to_today")]
+    #[serde(default = "today")]
     pub updated: chrono::NaiveDate,
 
     pub slug: String,
@@ -28,86 +27,52 @@ pub struct Post {
 
 #[derive(Serialize, Clone, Deserialize, Debug)]
 struct RawPost {
-    #[serde(default)]
-    pub uuid: Uuid,
-    #[serde(default)]
-    #[serde(deserialize_with = "bool_from_int")]
-    pub draft: bool,
+    pub uuid: Vec<u8>,
+    pub draft: i64,
     pub title: String,
-    pub date: chrono::NaiveDate,
-    #[serde(default = "default_to_today")]
-    pub updated: chrono::NaiveDate,
+    pub date: String,
+    pub updated: String,
     pub slug: String,
     pub tags: String,
     pub content: Option<String>,
 }
 
-fn default_to_today() -> chrono::NaiveDate {
+fn today() -> chrono::NaiveDate {
     chrono::offset::Local::today().naive_local()
-}
-
-/// Used to convert 0 and 1 to false and true, respectively
-fn bool_from_int<'de, D>(deserializer: D) -> Result<bool, D::Error>
-where
-    D: serde::Deserializer<'de>,
-{
-    // let test = 0 as u8;
-    // TODO change back to u8 once you insert it into
-    match i64::deserialize(deserializer)? {
-        0 => Ok(false),
-        1 => Ok(true),
-        other => Err(serde::de::Error::invalid_value(
-            serde::de::Unexpected::Unsigned(other as u64),
-            &"zero or one",
-        )),
-    }
 }
 
 impl From<RawPost> for Post {
     fn from(raw_post: RawPost) -> Self {
         Post {
-            uuid: raw_post.uuid,
-            draft: raw_post.draft,
+            uuid: Uuid::from_slice(&raw_post.uuid).expect("could not parse uuid"),
             title: raw_post.title,
-            date: raw_post.date,
-            updated: raw_post.updated,
+            draft: match &raw_post.draft {
+                0 => false,
+                1 => true,
+                _ => panic!(),
+            },
+            date: chrono::NaiveDate::parse_from_str(&raw_post.date, "%Y-%m-%d")
+                .expect("failed to parse date string"),
+            updated: chrono::NaiveDate::parse_from_str(&raw_post.updated, "%Y-%m-%d")
+                .expect("failed to parse date string"),
             slug: raw_post.slug,
-            tags: raw_post
-                .tags
-                .split(',')
-                .map(|str| str.to_owned())
-                .collect::<Vec<String>>(),
+            tags: raw_post.tags.split(',').map(|str| str.to_owned()).collect(),
             content: raw_post.content,
         }
     }
 }
 
 impl Post {
-    /// a debug method used to create arbitrary posts
-    pub fn _new(title: &str, date: &str, slug: &str, content: &str) -> Self {
-        Self {
-            uuid: Uuid::new_v4(),
-            title: title.to_string(),
-            draft: false,
-            date: chrono::NaiveDate::parse_from_str(&date, "%Y-%m-%d")
-                .expect("failed to parse date string"),
-            updated: default_to_today(),
-            slug: slug.to_string(),
-            tags: Vec::new(),
-            content: Some(content.to_string()),
-        }
-    }
-
     pub async fn all(pool: &SqlitePool) -> Result<Vec<Post>, sqlx::Error> {
         let posts = sqlx::query_as!(
             RawPost,
             r#"
             SELECT 
-                uuid as "uuid!: uuid::Uuid", 
-                draft as "draft!: bool", 
+                uuid,
+                draft,
                 title, 
-                date as "date!: chrono::NaiveDate", 
-                updated as "updated!: chrono::NaiveDate", 
+                date,
+                updated,
                 slug, 
                 tags, 
                 content 
@@ -166,54 +131,36 @@ impl Post {
         slug: String,
         pool: &SqlitePool,
     ) -> Result<Post, sqlx::Error> {
-        let post: Post = Post::from(
-            sqlx::query_as!(
-                RawPost,
-                r#"
-            SELECT 
-                uuid as "uuid!: uuid::Uuid", 
-                draft as "draft!: bool", 
-                title, 
-                date as "date!: chrono::NaiveDate", 
-                updated as "updated!: chrono::NaiveDate", 
-                slug, 
-                tags,
-                content 
+        let post: Post = sqlx::query_as!(
+            RawPost,
+            r#"
+            SELECT uuid, draft, title, date, updated, slug, tags, content 
             FROM posts
             WHERE date=$1 AND slug=$2
             "#,
-                date,
-                slug,
-            )
-            .fetch_one(pool)
-            .await?,
-        );
+            date,
+            slug,
+        )
+        .fetch_one(pool)
+        .await?
+        .into();
 
         Ok(post)
     }
 
     pub async fn get_with_uuid(uuid: Uuid, pool: &SqlitePool) -> Result<Post, sqlx::Error> {
-        let post = Post::from(
-            sqlx::query_as!(
-                RawPost,
-                r#"
-            SELECT 
-                uuid as "uuid!: uuid::Uuid", 
-                draft as "draft!: bool", 
-                title, 
-                date as "date!: chrono::NaiveDate", 
-                updated as "updated!: chrono::NaiveDate", 
-                slug, 
-                tags,
-                content 
+        let post: Post = sqlx::query_as!(
+            RawPost,
+            r#"
+            SELECT uuid, draft, title, date, updated, slug, tags, content 
             FROM posts
             WHERE uuid=$1
             "#,
-                uuid
-            )
-            .fetch_one(pool)
-            .await?,
-        );
+            uuid
+        )
+        .fetch_one(pool)
+        .await?
+        .into();
 
         Ok(post)
     }
